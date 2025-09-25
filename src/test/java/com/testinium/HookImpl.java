eee0f4a649597574ab9b2f91b406025fe1b98d36
mehmetaksahin <mehmet.aksahin@testinium.com>
2025-09-25 09:36:27 +0300

    Access Key Changed

--- src/test/java/com/testinium/HookImpl.java
+++ src/test/java/com/testinium/HookImpl.java
@@ -97,7 +97,7 @@ public class HookImpl {
             //String hubURL = "http://host.docker.internal:4444/wd/hub";
             String hubURL = "http://hub.testinium.io/wd/hub";
             DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("key", "qa63:a0c767b1e729c52c077f842a0363322a");
+            capabilities.setCapability("key", "fkartal:8a0e19c3383c5034fb86d3b93f5270a8");
             System.out.println("key:" + key);
             System.out.println("platform" + System.getenv("platform"));
             System.out.println("version" + System.getenv("version"));
